933b8f320bfc77225d3d3bf510a2d04a26f08e72
trick47 <60082733+trick47@users.noreply.github.com>
2023-03-01 09:26:17 -0500

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'bitsport_arise' );
+define( 'DB_NAME', 'zpberbaefp' );
 
 /** MySQL database username */
-define( 'DB_USER', 'bitsport_arise' );
+define( 'DB_USER', 'zpberbaefp' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'TapTap123@#' );
+define( 'DB_PASSWORD', 'tf6ExKjNtd' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
